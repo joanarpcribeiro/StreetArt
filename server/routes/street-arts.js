33058c5549f53237f4d1655756f80ff1ff61ce2f
Joana Duarte <joanarpcribeiro@yahoo.com>
2019-05-08 10:53:44 +0100

    Iteration 4

--- server/routes/street-arts.js
+++ server/routes/street-arts.js
@@ -2,11 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const StreetArt= require('../models/StreetArt')
+//import axios from 'axios';
 
 router.get('/', (req, res, next) => {
   StreetArt.find()
     .then( streetArt => {
       res.json(streetArt);
+    })
+    .catch(err => next(err))
+});
+
+router.get('/:streetArtId', (req, res, next) => {
+  StreetArt.findById(req.params.streetArtId)
+    .then( art => {
+      res.json(art)
     })
     .catch(err => next(err))
 });
