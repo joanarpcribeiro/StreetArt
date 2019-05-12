42f50010fbda4fbf81b20528b1b3731d4c07deac
Joana Duarte <joanarpcribeiro@yahoo.com>
2019-05-12 17:08:43 +0100

    Iteration 13, trying to display the map with marker

--- client/src/components/pages/StreetArtDetail.jsx
+++ client/src/components/pages/StreetArtDetail.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../api.js';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
 
 export default class StreetArtDetail extends Component {
   constructor(props) {
@@ -7,22 +8,39 @@ export default class StreetArtDetail extends Component {
     this.state = {
       streetArt: null
     };
+    this.mapRef = React.createRef() // NEW
+    this.map = null // NEW
+    this.marker = null // NEW
   }
-  returnDetailOfOneArt = () =>{
-    api.getStreetArt(this.props.match.params.streetArtId)
-      .then(streetArt => {
-        console.log("RESPONSE FROM API", streetArt)
-        this.setState({
-          streetArt: streetArt
-        })
-      })
-      .catch((err)=>{
-        console.log("The following error is showing", err)
-      })
+  initMap(lng, lat) {
+    // Embed the map where "this.mapRef" is defined in the render
+    this.map = new mapboxgl.Map({
+      container: this.mapRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: 10
+    })
+    this.map.addControl(new mapboxgl.NavigationControl())
   }
-  componentDidMount(){
-    this.returnDetailOfOneArt();
+  addMarker(streetArt) {
+    this.marker = new mapboxgl.Marker({color: 'red'})
+      .setLngLat(streetArt.location.coordinates[1],streetArt.location.coordinates[0])
+      .addTo(this.map)
   }
+  //returnDetailOfOneArt = () =>{
+  //  api.getStreetArt(this.props.match.params.streetArtId)
+  //    .then(streetArt => {
+  //      console.log("RESPONSE FROM API", streetArt)
+  //      this.setState({
+  //        streetArt: streetArt
+  //      })
+  //      let [lng,lat] = streetArt.location.coordinates // NEW
+  //      this.initMap(lng,lat)
+  //    })
+  //    .catch((err)=>{
+  //      console.log("The following error is showing", err)
+  //    })
+  //}
   render() {
     if (!this.state.streetArt) return <div>Loading...</div>
     console.log(this.state.pictureUrl)                
@@ -31,8 +49,22 @@ export default class StreetArtDetail extends Component {
         <h2><img src={this.state.streetArt.pictureUrl} /></h2>
         <p><strong>Longitude:</strong> {this.state.streetArt.location.coordinates[1]}</p>
         <p><strong>Latitude:</strong> {this.state.streetArt.location.coordinates[0]}</p>
-
+        <div ref={this.mapRef} style={{height: 400}}></div>
       </div>
     );
+  }
+  componentDidMount(){
+    api.getStreetArt(this.props.match.params.streetArtId)
+     .then(streetArt => {
+       console.log("RESPONSE FROM API", streetArt)
+       this.setState({
+         streetArt: streetArt
+       })
+       let [lng,lat] = streetArt.location.coordinates // NEW
+       this.initMap(lng,lat) 
+     })
+    .catch((err)=>{
+      console.log("The following error is showing", err)
+    })
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App.jsx';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
 // import registerServiceWorker from './registerServiceWorker';
+
+// Inform your Mapbox token (https://www.mapbox.com/account/)
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9hbmFycGNyaWJlaXJvIiwiYSI6ImNqdWwxZnB0dDE2bnM0OW43ZHpsaGE3ODAifQ.xY8Q8NmmyKNthb-w-XBFMg'
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 // registerServiceWorker();
