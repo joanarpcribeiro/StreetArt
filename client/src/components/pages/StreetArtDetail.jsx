0e58568b4e7ff04fa24a56db45e940304f138224
Joana Duarte <joanarpcribeiro@yahoo.com>
2019-05-13 09:10:11 +0100

    Iteration 13 DONE

--- client/src/components/pages/StreetArtDetail.jsx
+++ client/src/components/pages/StreetArtDetail.jsx
@@ -21,35 +21,22 @@ export default class StreetArtDetail extends Component {
       zoom: 10
     })
     this.map.addControl(new mapboxgl.NavigationControl())
-  }
-  addMarker(streetArt) {
     this.marker = new mapboxgl.Marker({color: 'red'})
-      .setLngLat(streetArt.location.coordinates[1],streetArt.location.coordinates[0])
-      .addTo(this.map)
+    .setLngLat([lng,lat])
+    .addTo(this.map)
   }
-  //returnDetailOfOneArt = () =>{
-  //  api.getStreetArt(this.props.match.params.streetArtId)
-  //    .then(streetArt => {
-  //      console.log("RESPONSE FROM API", streetArt)
-  //      this.setState({
-  //        streetArt: streetArt
-  //      })
-  //      let [lng,lat] = streetArt.location.coordinates // NEW
-  //      this.initMap(lng,lat)
-  //    })
-  //    .catch((err)=>{
-  //      console.log("The following error is showing", err)
-  //    })
-  //}
   render() {
     if (!this.state.streetArt) return <div>Loading...</div>
     console.log(this.state.pictureUrl)                
     return (
+      
       <div className="StreetArtDetail">
         <h2><img src={this.state.streetArt.pictureUrl} /></h2>
-        <p><strong>Longitude:</strong> {this.state.streetArt.location.coordinates[1]}</p>
-        <p><strong>Latitude:</strong> {this.state.streetArt.location.coordinates[0]}</p>
-        <div ref={this.mapRef} style={{height: 400}}></div>
+        {/* <p><strong>Longitude:</strong> {this.state.streetArt.location.coordinates[1]}</p>
+        <p><strong>Latitude:</strong> {this.state.streetArt.location.coordinates[0]}</p> */}
+        <div className="map">
+          <div ref={this.mapRef} style={{height: 400}}></div>
+        </div>
       </div>
     );
   }
